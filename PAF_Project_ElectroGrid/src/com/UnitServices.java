ede2f298ff3bf680d0cec358dfca7749e356e7b5
miuninihara001 <miuniniharae001@gmail.com>
2022-04-22 00:25:32 +0530

    unit reading insert commit

--- PAF_Project_ElectroGrid/src/com/UnitServices.java
+++ PAF_Project_ElectroGrid/src/com/UnitServices.java
@@ -1,5 +1,44 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import model.Units;
+
+@Path("/unit") 
+
 public class UnitServices {
 
+
+	Units readObj = new Units();
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertItem(@FormParam("accountNo") String accountNo,
+	 @FormParam("consumerName") String consumerName,
+	 @FormParam("address") String address,
+	 @FormParam("consumedUnits") Integer consumedUnits,
+	 @FormParam("date") String date)
+	{
+	 String output = readObj.insertRecords(accountNo, consumerName, address, consumedUnits,date);
+	return output;
+	}
+
+	
 }
--- PAF_Project_ElectroGrid/src/model/Units.java
+++ PAF_Project_ElectroGrid/src/model/Units.java
@@ -1,5 +1,71 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Units {
+	
+	//A common method to connect to the DB
+	private Connection connect()
+	 {
+		Connection con = null;
+	 try
+	 {
+		 Class.forName("com.mysql.jdbc.Driver");
+
+		 //Provide the correct details: DBServer/DBName, username, password
+		 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/eg", "root", "");
+	 }
+	 catch (Exception e)
+	 {
+		 e.printStackTrace();
+	 }
+	 return con;
+	 } 
+	
+	
+
+	public String insertRecords(String acc, String name, String address, Integer units, String date)
+	 {
+		String output = "";
+	 try
+	 {
+		 Connection con = connect();
+		 if (con == null)
+		 {
+			 return "Error while connecting to the database for inserting !"; 
+		 }
+		 
+		 	// create a prepared statement
+		 	String query = " insert into unitrecords(`recordID`,`accountNo`,`consumerName`,`address`,`consumedUnits`, `date`)"
+		 	+ " values (?, ?, ?, ?, ?, ?)";
+		 	
+		 	PreparedStatement preparedStmt = con.prepareStatement(query);
+		 	// binding values
+		 	preparedStmt.setInt(1, 0);
+		 	preparedStmt.setString(2, acc);
+	 		preparedStmt.setString(3, name);
+	 		preparedStmt.setString(4, address);
+	 		preparedStmt.setInt(5, units);
+	 		preparedStmt.setString(6, date);
+	 		
+	 		// execute the statement
+	 
+	 		preparedStmt.execute();
+	 		con.close();
+	 		output = "Inserted successfully";
+	 }
+	 catch (Exception e)
+	 {
+		 	output = "Error while inserting the record !";
+		 	System.err.println(e.getMessage());
+	 }
+	 
+	 return output;
+	 
+	 }
 
 }
