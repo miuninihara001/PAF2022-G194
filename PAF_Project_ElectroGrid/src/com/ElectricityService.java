bfbd65ba7065e825936926131c628bc7539ac432
kaveendhya <chamodikaveendya1999@gmail.com>
2022-04-22 15:52:22 +0530

    second commit-customer

--- PAF_Project_ElectroGrid/src/com/ElectricityService.java
+++ PAF_Project_ElectroGrid/src/com/ElectricityService.java
@@ -1,5 +1,45 @@
 package com;
 
+import javax.ws.rs.Consumes;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Electricity; 
+
+@Path("/electric")
 public class ElectricityService {
+	
+	Electricity eleconnec = new Electricity();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readCons()
+	 {
+		return "hell";
+	 } 
+
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertCon(@FormParam("AccountNum") String AccountNum,
+	 @FormParam("LineNum") String LineNum,
+	 @FormParam("ClientName") String ClientName,
+	 @FormParam("NIC") String NIC,
+	 @FormParam("ConnecType") String ConnecType,
+	 @FormParam("Email") String Email,
+	 @FormParam("Address") String Address,
+	 @FormParam("WiringType") String WiringType)
+	{
+	 String output = eleconnec.insertCon(AccountNum, LineNum, ClientName, NIC,ConnecType,Email,Address,WiringType);
+	return output;
+	}
+	
 
 }
--- PAF_Project_ElectroGrid/src/model/Electricity.java
+++ PAF_Project_ElectroGrid/src/model/Electricity.java
@@ -1,6 +1,64 @@
 package model;
 
+import java.sql.*;
+
 public class Electricity {
+	
+	private Connection connect()
+	 {
+	 Connection con = null;
+	 try
+	 {
+	 Class.forName("com.mysql.jdbc.Driver");
+
+	 //Provide the correct details: DBServer/DBName, username, password
+	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/electrogrid", "root", "");
+	 }
+	 catch (Exception e)
+	 {e.printStackTrace();}
+	 return con;
+	 } 
+	
+	
+	public String insertCon(String AccountNum, String LineNum, String ClientName, String NIC, String ConnecType, String Email, String Address, String WiringType)
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for inserting."; }
+	 // create a prepared statement
+	 String query = " insert into connections(`ConCode`,`AccountNum`,`LineNum`,`ClientName`,`NIC`,`ConnecType`,`Email`,`Address`,`WiringType`)"
+	 + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 preparedStmt.setInt(1, 0);
+	 preparedStmt.setString(2, AccountNum);
+	 preparedStmt.setString(3, LineNum);
+	 preparedStmt.setString(4, ClientName);
+	 preparedStmt.setString(5, NIC);
+	 preparedStmt.setString(6, ConnecType);
+	 preparedStmt.setString(7, Email);
+	 preparedStmt.setString(8, Address);
+	 preparedStmt.setString(9, WiringType);
+	 // execute the statement
+	 
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Inserted successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while inserting the item.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 } 
+	
+	
+	
+	
 	
 
 }
