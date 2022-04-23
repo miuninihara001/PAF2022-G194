7a08844431f3974d47954fb3744dae50454323bb
Sanduni Tharuka <Sanduni Tharuka@DESKTOP-F313J2Q>
2022-04-24 01:03:26 +0530

    commit 2

--- PAF_Project_ElectroGrid/src/com/CunsumptionService.java
+++ PAF_Project_ElectroGrid/src/com/CunsumptionService.java
@@ -1,5 +1,28 @@
 package com;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Cunsumption;
+
+@Path("/Cunsumption") 
+
 public class CunsumptionService {
+   
+	
+
+	
+		
+		Cunsumption consumpObj = new Cunsumption(); 
+		@GET
+		@Path("/") 
+		@Produces(MediaType.TEXT_HTML) 
+		public String readCunsumption() 
+		 { 
+			return "Hello";
+			//return consumpObj.readCunsumption();
+		 } 
 
 }
--- PAF_Project_ElectroGrid/src/model/Cunsumption.java
+++ PAF_Project_ElectroGrid/src/model/Cunsumption.java
@@ -1,5 +1,57 @@
 package model;
 
-public class Cunsumption {
+import java.sql.*; 
 
+public class Cunsumption 
+{ //A common method to connect to the DB
+  private Connection connect() 
+ { 
+ Connection con = null; 
+ try
+ { 
+ Class.forName("com.mysql.jdbc.Driver"); 
+ 
+ //Provide the correct details: DBServer/DBName, username, password 
+ con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf", "root", ""); 
+ } 
+ catch (Exception e) 
+ {e.printStackTrace();} 
+ return con; 
+ } 
+public String insertconsumption(String Dname, String DZipCode, String DUsedUnits, String Note) 
+ { 
+ String output = ""; 
+ try
+ { 
+ Connection con = connect(); 
+ if (con == null) 
+ {return "Error while connecting to the database for inserting."; } 
+ // create a prepared statement
+ String query = " insert into consumption(`ID`,`Dname`,`String DZipCode`,`DUsedUnits`,`Note`)"
+ + " values (?, ?, ?, ?, ?)"; 
+ PreparedStatement preparedStmt = con.prepareStatement(query); 
+ // binding values
+ preparedStmt.setInt(1, 0); 
+ preparedStmt.setString(2, Dname); 
+ preparedStmt.setString(3, DZipCode); 
+ preparedStmt.setDouble(4, Double.parseDouble(DUsedUnits)); 
+ preparedStmt.setString(5, Note); 
+ 
+//execute the statement
+
+ preparedStmt.execute(); 
+ con.close(); 
+ output = "Inserted successfully"; 
+ } 
+ catch (Exception e) 
+ { 
+ output = "Error while inserting the Details."; 
+ System.err.println(e.getMessage()); 
+ } 
+ return output; 
+ }
+public String readCunsumption() {
+	// TODO Auto-generated method stub
+	return null;
+} 
 }
