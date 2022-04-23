950dbc080bb71d36b27abb5b1667f91efe45b3c2
asus <omalaVK>
2022-04-23 12:17:59 +0530

    # WARNING: head commit changed in the meantime
    schedule commit3

--- PAF_Project_ElectroGrid/src/com/ScheduleService.java
+++ PAF_Project_ElectroGrid/src/com/ScheduleService.java
@@ -1,6 +1,7 @@
 package com;
 import model.Schedule;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -8,6 +9,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -70,4 +75,19 @@ public class ScheduleService {
 		return output;
 	}
 	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteShedule(String sheduleData)
+	{
+		//Convert the input string to an XML document
+		Document doc = Jsoup.parse(sheduleData, "", Parser.xmlParser());
+		//Read the value from the element <sheduleId>
+		String scheduleId = doc.select("sheduleId").text();
+		
+		String output = scheduleObj.deleteShedule(scheduleId);
+		
+		return output;
+	}
 }
