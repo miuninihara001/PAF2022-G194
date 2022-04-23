c99203cdd12dbb94a6ade73c1f4451ec7a3a0bc0
kaveendhya <chamodikaveendya1999@gmail.com>
2022-04-23 16:29:25 +0530

    third commits-customer

--- PAF_Project_ElectroGrid/src/com/ElectricityService.java
+++ PAF_Project_ElectroGrid/src/com/ElectricityService.java
@@ -1,13 +1,21 @@
 package com;
 
 import javax.ws.rs.Consumes;
-
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import model.Electricity; 
 
@@ -20,7 +28,7 @@ public class ElectricityService {
 	@Produces(MediaType.TEXT_HTML)
 	public String readCons()
 	 {
-		return "hell";
+		return eleconnec.readCons();
 	 } 
 
 	
@@ -40,6 +48,48 @@ public class ElectricityService {
 	 String output = eleconnec.insertCon(AccountNum, LineNum, ClientName, NIC,ConnecType,Email,Address,WiringType);
 	return output;
 	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateCode(String ConData)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject ConObject = new JsonParser().parse(ConData).getAsJsonObject();
+	//Read the values from the JSON object
+	 String ConCode = ConObject.get("ConCode").getAsString();
+	 String AccountNum = ConObject.get("AccountNum").getAsString();
+	 String LineNum = ConObject.get("LineNum").getAsString();
+	 String ClientName = ConObject.get("ClientName").getAsString();
+	 String NIC = ConObject.get("NIC").getAsString();
+	 String ConnecType = ConObject.get("ConnecType").getAsString();
+	 String Email = ConObject.get("Email").getAsString();
+	 String Address = ConObject.get("Address").getAsString();
+	 String WiringType = ConObject.get("WiringType").getAsString();
+	 
+	 String output = eleconnec.updateCon(ConCode, AccountNum, LineNum, ClientName, NIC,ConnecType,Email,Address,WiringType);
+	 
+	return output;
+	}
+	
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteCons(String ConData)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(ConData, "", Parser.xmlParser());
+
+	//Read the value from the element <itemID>
+	 String ConCode = doc.select("ConCode").text();
+	 String output = eleconnec.deleteCons(ConCode);
+	return output;
+	}
+
+	
 	
 
 }
--- PAF_Project_ElectroGrid/src/model/Electricity.java
+++ PAF_Project_ElectroGrid/src/model/Electricity.java
@@ -56,6 +56,123 @@ public class Electricity {
 	 return output;
 	 } 
 	
+	public String readCons()
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for reading."; }
+	 // Prepare the html table to be displayed
+	 output = "<table border='1'><tr><th>Account Number</th><th>Line Number</th>" +
+	 "<th>Client Name</th>" +
+	 "<th>NIC</th>"+" <th>Connection Type</th>"+" <th>Email</th>"+" <th>Address</th>"+" <th>Wiring Type</th>" +
+	 "<th>Update</th><th>Remove</th></tr>";
+
+	 String query = "select * from connections";
+	 Statement stmt = con.createStatement();
+	 ResultSet rs = stmt.executeQuery(query);
+	 // iterate through the rows in the result set
+	 while (rs.next())
+	 {
+	 String AccountNum = rs.getString("AccountNum");
+	 String LineNum = rs.getString("LineNum");
+	 String ClientName = rs.getString("ClientName");
+	 String NIC = rs.getString("NIC");
+	 String ConnecType = rs.getString("ConnecType");
+	 String Email = rs.getString("Email");
+	 String Address = rs.getString("Address");
+	 String WiringType = rs.getString("WiringType");
+	 
+	 // Add into the html table
+	 output += "<tr><td>" + AccountNum + "</td>";
+	 output += "<td>" + LineNum + "</td>";
+	 output += "<td>" + ClientName + "</td>";
+	 output += "<td>" + NIC + "</td>";
+	 output += "<td>" + ConnecType + "</td>";
+	 output += "<td>" + Email + "</td>";
+	 output += "<td>" + Address + "</td>";
+	 output += "<td>" + WiringType + "</td>";
+	 // buttons
+	 output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
+	 + "<td><form method='post' action='items.jsp'>"
+	+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+	 + "<input name='AccountNum' type='hidden' value='" + AccountNum + "'>" + "</form></td></tr>";
+	 }
+	 con.close();
+	 // Complete the html table
+	 output += "</table>";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while reading the items.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 } 
+	
+	
+	
+	public String updateCon(String ConCode,String AccountNum, String LineNum, String ClientName, String NIC, String ConnecType, String Email, String Address, String WiringType)
+	{
+		 String output = "";
+		 try
+		 {
+		 Connection con = connect();
+		 if (con == null)
+		 {return "Error while connecting to the database for updating."; }
+		 // create a prepared statement
+		 String query = "UPDATE connections SET AccountNum=?,LineNum=?,ClientName=?,NIC=?,ConnecType=?,Email=?,Address=?,WiringType=? WHERE ConCode=?";
+		 PreparedStatement preparedStmt = con.prepareStatement(query);
+		 // binding values
+		 preparedStmt.setString(1, AccountNum);
+		 preparedStmt.setString(2, LineNum);
+		 preparedStmt.setString(3, ClientName);
+		 preparedStmt.setString(4, NIC);
+		 preparedStmt.setString(5, ConnecType);
+		 preparedStmt.setString(6, Email);
+		 preparedStmt.setString(7, Address);
+		 preparedStmt.setString(8, WiringType);
+		 preparedStmt.setInt(9, Integer.parseInt(ConCode));
+		 // execute the statement
+		 preparedStmt.execute();
+		 con.close();
+		 output = "Updated successfully";
+		 }
+		 catch (Exception e)
+		 {
+		 output = "Error while updating the item.";
+		 System.err.println(e.getMessage());
+		 }
+		 return output;
+		 } 
+	
+	public String deleteCons(String ConCode)
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for deleting."; }
+	 // create a prepared statement
+	 String query = "delete from connections where ConCode=?";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 preparedStmt.setInt(1, Integer.parseInt(ConCode));
+	 // execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Deleted successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while deleting the item.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
 	
 	
 	
