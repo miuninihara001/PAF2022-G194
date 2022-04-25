84950c1e97c0da08cebd00abe2f23ee9dd1766cc
Sanduni Tharuka <Sanduni Tharuka@DESKTOP-F313J2Q>
2022-04-25 20:25:31 +0530

    table linking commit

--- PAF_Project_ElectroGrid/src/com/CunsumptionService.java
+++ PAF_Project_ElectroGrid/src/com/CunsumptionService.java
@@ -1,5 +1,5 @@
 package com;
-
+//
 //import javax.swing.text.Document;
 //import javax.ws.rs.Consumes;
 //import javax.ws.rs.DELETE;
@@ -12,7 +12,7 @@ package com;
 //import javax.ws.rs.core.MediaType;
 //import org.jsoup.parser.*;
 //import org.jsoup.Jsoup;
-//
+
 //
 //
 //import com.google.gson.JsonObject;
@@ -20,9 +20,9 @@ package com;
 //import com.google.protobuf.Parser;
 //
 import model.Cunsumption;
-
-
-
+//import model.GET;
+//import model.Path;
+//import model.Produces;
 
 // For REST service
 import javax.ws.rs.*;
@@ -47,32 +47,57 @@ public class CunsumptionService {
 		
 		Cunsumption consumpObj = new Cunsumption(); 
 		@GET
-		@Path("/") 
+		@Path("/readCunsumption") 
 		@Produces(MediaType.TEXT_HTML) 
 		public String readCunsumption() 
 		 { 
 		
-		return consumpObj.readCunsumption();
+		   return consumpObj.readCunsumption();
 		 } 
 		
 		@POST
-		@Path("/") 
+		@Path("/insertConsumption") 
 		@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
 		@Produces(MediaType.TEXT_PLAIN) 
 		
-		public String insertconsumption(@FormParam("Dname") String Dname, 
-		   @FormParam("DZipCode") String DZipCode, 
-		   @FormParam("DUsedUnits") String DUsedUnits, 
-		   @FormParam("Month") String Month,
-		   @FormParam("Note") String Note) 
-		{ 
-		 String output = consumpObj.insertconsumption(Dname, DZipCode, DUsedUnits, Month, Note); 
-		 return output; 
+		public String insertconsumption
+		(@FormParam("Dname") String Dname, 
+		 @FormParam("Month") String Month,
+		 @FormParam("Note") String Note) 
+		
+		//inserting validations
+		{
+			if(Dname.isEmpty()||Month.isEmpty()||Note.isEmpty())
+			{
+				 return "All fields must be filled out";
+			}
+		
+			else if(Dname.length()<=4) {
+				 return "Dname length must be 4 or more characters long";
+			 }
+			
+			else if(Month.length()<=3) {
+				 return "Month length must be 3 or more characters long";
+			 }
+			
+			String output = consumpObj.insertconsumption(Dname,Month,Note);
+			return output;
 		}
+		
+		 
+		@GET
+		@Path("/searchCunsumption")
+		@Produces(MediaType.TEXT_HTML)
+		public String searchSchedules(String consumptionData) {
+		Document doc = Jsoup.parse(consumptionData, "", Parser.xmlParser());
+		String Dname = doc.select("Dname").text();
+		return consumpObj.searchCunsumption(Dname);
+		}
+
 
 		
 		@PUT
-		@Path("/") 
+		@Path("/updateCunsumption") 
 		@Consumes(MediaType.APPLICATION_JSON) 
 		@Produces(MediaType.TEXT_PLAIN) 
 		public String updateCunsumption(String consumptionData) 
@@ -87,19 +112,19 @@ public class CunsumptionService {
 		//Read the values from the JSON object
 		 String ID =ConObject1.get("ID").getAsString(); 
 		 String Dname = ConObject1.get("Dname").getAsString(); 
-		 String DZipCode = ConObject1.get("DZipCode").getAsString(); 
-		 String DUsedUnits = ConObject1.get("DUsedUnits").getAsString(); 
+//		 String DZipCode = ConObject1.get("DZipCode").getAsString(); 
+//		 String DUsedUnits = ConObject1.get("DUsedUnits").getAsString(); 
 		 String Month = ConObject1.get("Month").getAsString();
 		 String Note = ConObject1.get("Note").getAsString(); 
 		 
-		 String output = consumpObj.updateCunsumption(ID, Dname, DZipCode, DUsedUnits,Month, Note); 
+		 String output = consumpObj.updateCunsumption(ID, Dname,Month, Note); 
 		 return output; 
 		}
 		
 		
 		
 		@DELETE
-		@Path("/") 
+		@Path("/deleteConsumption") 
 		@Consumes(MediaType.APPLICATION_XML) 
 		@Produces(MediaType.TEXT_PLAIN) 
 		public String deleteCunsumption(String consumptionData) 
@@ -107,23 +132,26 @@ public class CunsumptionService {
 		//Convert the input string to an XML document
 		 Document doc = Jsoup.parse(consumptionData, "", Parser.xmlParser()); 
 		 
-		//Read the value from the element <itemID>
+		//Read the value from the element <ID>
 		 String ID = doc.select("ID").text(); 
 		 
 		 String output = consumpObj.deleteCunsumption(ID); 
 		return output; 
 		}
 	
-		//search
 		
-		@GET
-		@Path("/search")
-		@Produces(MediaType.TEXT_HTML)
-		public String searchCunsumption(String consumptionData) {
-			Document doc = Jsoup.parse(consumptionData, "", Parser.xmlParser()); 
-			String ID = doc.select("Dname").text(); 
-			
-			return consumpObj.searchCunsumption(Dname);
-		}
+		
+//		//search
+//		
+		 @GET
+			@Path("/searchCunsumption")
+			@Produces(MediaType.TEXT_HTML)
+			public String searchCunsumption(String consumptionData) {
+				Document doc = Jsoup.parse(consumptionData, "", Parser.xmlParser()); 
+				String ID = doc.select("ID").text(); 
+				
+				return consumpObj.searchCunsumption(ID);
+			}
+
 		
 }
