21f02cf03c539008ffabeae127a8c7a29adcd445
asus <omalaVK>
2022-04-25 22:05:43 +0530

    poer cut commit8

--- PAF_Project_ElectroGrid/src/model/Schedule.java
+++ PAF_Project_ElectroGrid/src/model/Schedule.java
@@ -23,34 +23,34 @@ public class Schedule {
 	public String InsertPowerCutDetails(String lineno, String areano, String areaname, String starttime, String endtime,
 			String date, String reason) {
 		String output = "";
-		
+
 		try {
-			Connection con = connect();
-			if (con == null) {
-				return "Error while connecting to the DB for inserting";
-			}
+				Connection con = connect();
+				if (con == null) {
+					return "Error while connecting to the DB for inserting";
+				}
 
-			String query = " insert into shedules (`sheduleId`,`lineNo`,`areaNo`,`areaName`,`startTime`,`endTime`,`date`,`reason`)"
-					+ " values (?, ?, ?, ?, ?, ?, ?, ?)";
-			PreparedStatement preparedStat = con.prepareStatement(query);
-			// values binding
-			preparedStat.setInt(1, 0);
-			preparedStat.setString(2, lineno);
-			preparedStat.setString(3, areano);
-			preparedStat.setString(4, areaname);
-			preparedStat.setString(5, starttime);
-			preparedStat.setString(6, endtime);
-			preparedStat.setString(7, date);
-			preparedStat.setString(8, reason);
-			// Execute statement
-
-			preparedStat.execute();
-			con.close();
-			output = "Power cut schedule details inserted successfully";
+				String query = " insert into shedules (`sheduleId`,`lineNo`,`areaNo`,`areaName`,`startTime`,`endTime`,`date`,`reason`)"
+						+ " values (?, ?, ?, ?, ?, ?, ?, ?)";
+				PreparedStatement preparedStat = con.prepareStatement(query);
+				// values binding
+				preparedStat.setInt(1, 0);
+				preparedStat.setString(2, lineno);
+				preparedStat.setString(3, areano);
+				preparedStat.setString(4, areaname);
+				preparedStat.setString(5, starttime);
+				preparedStat.setString(6, endtime);
+				preparedStat.setString(7, date);
+				preparedStat.setString(8, reason);
+				// Execute statement
+	
+				preparedStat.execute();
+				con.close();
+				output = "Power cut schedule details inserted successfully";
 
 		} catch (Exception e) {
-			//output = "Error while inserting power cut shedule details";
-			output =  e.getMessage();
+			// output = "Error while inserting power cut shedule details";
+			output = e.getMessage();
 		}
 		return output;
 	}
@@ -58,43 +58,48 @@ public class Schedule {
 	public String readPowerCutDetails() {
 		String output = "";
 		try {
-			Connection con = connect();
-			if (con == null) {
-				return "Error while connecting to the DB for reading";
-			}
-//html table to be display
-			output = "<table border='1'><tr><th>Line Number</th><th>Area Number</th><th>Area Name</th>"
-					+ "<th>Start Time</th><th>End Time</th>" + "<th>Date</th><th>Reason</th></tr>";
-			String query = "select * from shedules";
-			Statement stat = con.createStatement();
-			ResultSet rs = stat.executeQuery(query);
-//iteration through the rows of result set
-			while (rs.next()) {
-				String sheduleId = Integer.toString(rs.getInt("sheduleId"));
-				String lineNo = rs.getString("lineNo");
-				String areaNo = rs.getString("areaNo");
-				String areaName = rs.getString("areaName");
-				String startTime = rs.getString("startTime");
-				String endTime = rs.getString("endTime");
-				String date = rs.getString("date");
-				String reason = rs.getString("reason");
-//add raws into the html table
-				output += "<tr><td>" + lineNo + "</td>";
-				output += "<td>" + areaNo + "</td>";
-				output += "<td>" + areaName + "</td>";
-				output += "<td>" + startTime + "</td>";
-				output += "<td>" + endTime + "</td>";
-				output += "<td>" + date + "</td>";
-				output += "<td>" + reason + "</td>";
-//buttons
-				output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
-						+ "<td><form method='post' action='shedules.jsp'>"
-						+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
-						+ "<input name='sheduleId' type='hidden' value='" + sheduleId + "'>" + "</form></td></tr>";
-			}
-			con.close();
-//complete html table
-			output += "</table>";
+				Connection con = connect();
+				if (con == null) {
+					return "Error while connecting to the DB for reading";
+				}
+				
+				//html table to be display
+				output = "<table border='1'><tr><th>Line Number</th><th>Area Number</th><th>Area Name</th>"
+						+ "<th>Start Time</th><th>End Time</th>" + "<th>Date</th><th>Reason</th></tr>";
+				String query = "select * from shedules";
+				Statement stat = con.createStatement();
+				ResultSet rs = stat.executeQuery(query);
+				
+				//iteration through the rows of result set
+				while (rs.next()) {
+					String sheduleId = Integer.toString(rs.getInt("sheduleId"));
+					String lineNo = rs.getString("lineNo");
+					String areaNo = rs.getString("areaNo");
+					String areaName = rs.getString("areaName");
+					String startTime = rs.getString("startTime");
+					String endTime = rs.getString("endTime");
+					String date = rs.getString("date");
+					String reason = rs.getString("reason");
+					//add raws into the html table
+					output += "<tr><td>" + lineNo + "</td>";
+					output += "<td>" + areaNo + "</td>";
+					output += "<td>" + areaName + "</td>";
+					output += "<td>" + startTime + "</td>";
+					output += "<td>" + endTime + "</td>";
+					output += "<td>" + date + "</td>";
+					output += "<td>" + reason + "</td>";
+					
+					//buttons
+					output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
+							+ "<td><form method='post' action='shedules.jsp'>"
+							+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+							+ "<input name='sheduleId' type='hidden' value='" + sheduleId + "'>" + "</form></td></tr>";
+				}
+				
+				con.close();
+				//complete html table
+				output += "</table>";
+	
 		} catch (Exception e) {
 			output = "Error while reading the power cut shedule details.";
 			System.err.println(e.getMessage());
@@ -110,10 +115,9 @@ public class Schedule {
 				return "Error while connecting to the database";
 			}
 			output = "<table border='1'><tr><th>Line No</th>" + "<th>Area No</th>" + "<th>Area Name</th>"
-					+ "<th>Starting Time</th>" + "<th>Ending Time</th>" + "<th>Date</th>" + "<th>Reason</th></tr>"; // create
-// a
-// prepared
-// statement
+					+ "<th>Starting Time</th>" + "<th>Ending Time</th>" + "<th>Date</th>" + "<th>Reason</th></tr>"; 
+			
+			// create a prepared statement
 			String query = "select * from shedules where lineNo='" + lineno + "'";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
@@ -149,10 +153,12 @@ public class Schedule {
 			if (con == null) {
 				return "Error while connecting to the database for updating.";
 			}
-// create a prepared statement
+			
+			// create a prepared statement
 			String query = "UPDATE shedules SET lineNo=?,areaNo=?, areaName=?, startTime=?, endTime=?, date=?, reason=? WHERE sheduleId=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
-// binding values
+			
+			// binding values
 			preparedStmt.setString(1, lineno);
 			preparedStmt.setString(2, areano);
 			preparedStmt.setString(3, areaname);
@@ -161,7 +167,8 @@ public class Schedule {
 			preparedStmt.setString(6, date);
 			preparedStmt.setString(7, reason);
 			preparedStmt.setInt(8, Integer.parseInt(ID));
-// execute the statement
+			
+			// execute the statement
 			preparedStmt.execute();
 			con.close();
 			output = "Updated successfully";
@@ -179,12 +186,15 @@ public class Schedule {
 			if (con == null) {
 				return "Error while connecting to the database for deleting.";
 			}
-// create a prepared statement
+			
+			// create a prepared statement
 			String query = "delete from shedules where sheduleId=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
-// binding values 
+			
+			// binding values 
 			preparedStmt.setInt(1, Integer.parseInt(sheduleId));
-// execute the statement
+			
+			// execute the statement
 			preparedStmt.execute();
 			con.close();
 			output = "Deleted successfully";
