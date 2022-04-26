3f60712613325fd13b166bbc1cf7e97810c48f5b
asus <omalaVK>
2022-04-26 16:08:22 +0530

    power cut commit9

--- PAF_Project_ElectroGrid/src/com/ScheduleService.java
+++ PAF_Project_ElectroGrid/src/com/ScheduleService.java
@@ -1,6 +1,6 @@
 package com;
-import model.Schedule;
 
+import model.Schedule;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -24,72 +24,61 @@ import org.jsoup.parser.Parser;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-
-
 @Path("/Schedule")
 public class ScheduleService {
-	
+
 	Schedule scheduleObj = new Schedule();
+
 	@GET
 	@Path("/")
 	@Produces(MediaType.TEXT_HTML)
-	public String readPowerCutDetails()
-	{
+	public String readPowerCutDetails() {
 		return scheduleObj.readPowerCutDetails();
 	}
-	
-	
+
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_PLAIN)
-	
-	public String InsertPowerCutDetails(@FormParam("lineNo") String lineNo,
-				@FormParam("areaNo") String areaNo,
-				@FormParam("areaName") String areaName,
-				@FormParam("startTime") String startTime,
-				@FormParam("endTime") String endTime,
-				@FormParam("date") String date,
-				@FormParam("reason") String reason) 
-	{
-			
-			if(lineNo.isEmpty()||areaNo.isEmpty()||areaName.isEmpty()||startTime.isEmpty()||endTime.isEmpty()||date.isEmpty()||reason.isEmpty()) {
-					return "All the Fields Should be inserted";			
-			}else if(lineNo.length() != 8) {
-					return "Line NO should consist of 8 characters";
-			}else if(areaNo.length() !=4) {
-					return "Area No should be consist of 4 characters ";
-			}
-			
-		
-		    	String output = scheduleObj.InsertPowerCutDetails(lineNo, areaNo, areaName, startTime, endTime, date, reason);
-		    	return output;
-		    
+
+	public String InsertPowerCutDetails(@FormParam("lineNo") String lineNo, @FormParam("areaNo") String areaNo,
+			@FormParam("areaName") String areaName, @FormParam("startTime") String startTime,
+			@FormParam("endTime") String endTime, @FormParam("date") String date, @FormParam("reason") String reason) {
+
+		if (lineNo.isEmpty() || areaNo.isEmpty() || areaName.isEmpty() || startTime.isEmpty() || endTime.isEmpty()
+				|| date.isEmpty() || reason.isEmpty()) {
+			return "All the Fields Should be inserted";
+		} else if (lineNo.length() != 8) {
+			return "Line NO should consist of 8 characters";
+		} else if (areaNo.length() != 4) {
+			return "Area No should be consist of 4 characters ";
+		}
+
+		String output = scheduleObj.InsertPowerCutDetails(lineNo, areaNo, areaName, startTime, endTime, date, reason);
+		return output;
+
 	}
-	
-	
+
 	@GET
 	@Path("/searchSchedule")
 	@Produces(MediaType.TEXT_HTML)
 	public String searchSchedules(String scheduleData) {
-		Document doc = Jsoup.parse(scheduleData, "", Parser.xmlParser()); 
-		String lineno = doc.select("lineno").text(); 
+		Document doc = Jsoup.parse(scheduleData, "", Parser.xmlParser());
+		String lineno = doc.select("lineno").text();
 		return scheduleObj.searchSchedules(lineno);
 	}
-	
-	
+
 	@PUT
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	
-	public String updateSchedule(String scheduleData)
-	{
-		//Convert the input string to a JSON object
+
+	public String updateSchedule(String scheduleData) {
+		// Convert the input string to a JSON object
 		JsonObject scheduleObject = new JsonParser().parse(scheduleData).getAsJsonObject();
-		
-		//Read the values from the JSON object
-		String sheduleId= scheduleObject.get("sheduleId").getAsString();
+
+		// Read the values from the JSON object
+		String sheduleId = scheduleObject.get("sheduleId").getAsString();
 		String lineNo = scheduleObject.get("lineNo").getAsString();
 		String areaNo = scheduleObject.get("areaNo").getAsString();
 		String areaName = scheduleObject.get("areaName").getAsString();
@@ -97,33 +86,33 @@ public class ScheduleService {
 		String endTime = scheduleObject.get("endTime").getAsString();
 		String date = scheduleObject.get("date").getAsString();
 		String reason = scheduleObject.get("reason").getAsString();
-		
-		
-		if(lineNo.isEmpty()||areaNo.isEmpty()||areaName.isEmpty()||startTime.isEmpty()||endTime.isEmpty()||date.isEmpty()||reason.isEmpty()) {
-			return "All the Fields Should be inserted";			
-		}else if(lineNo.length() != 8) {
+
+		if (lineNo.isEmpty() || areaNo.isEmpty() || areaName.isEmpty() || startTime.isEmpty() || endTime.isEmpty()
+				|| date.isEmpty() || reason.isEmpty()) {
+			return "All the Fields Should be inserted";
+		} else if (lineNo.length() != 8) {
 			return "Line NO should consist of 8 characters";
-		}else if(areaNo.length() !=4) {
+		} else if (areaNo.length() != 4) {
 			return "Area No should be consist of 4 characters ";
 		}
-		String output = scheduleObj.updateSchedule(sheduleId, lineNo, areaNo, areaName, startTime, endTime, date, reason );
-		
+		String output = scheduleObj.updateSchedule(sheduleId, lineNo, areaNo, areaName, startTime, endTime, date,
+				reason);
+
 		return output;
 	}
-	
+
 	@DELETE
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_XML)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String deleteShedule(String sheduleData)
-	{
-		//Convert the input string to an XML document
+	public String deleteShedule(String sheduleData) {
+		// Convert the input string to an XML document
 		Document doc = Jsoup.parse(sheduleData, "", Parser.xmlParser());
-		//Read the value from the element <sheduleId>
+		// Read the value from the element <sheduleId>
 		String scheduleId = doc.select("sheduleId").text();
-		
+
 		String output = scheduleObj.deleteShedule(scheduleId);
-		
+
 		return output;
 	}
 }
--- PAF_Project_ElectroGrid/src/model/Schedule.java
+++ PAF_Project_ElectroGrid/src/model/Schedule.java
@@ -25,28 +25,28 @@ public class Schedule {
 		String output = "";
 
 		try {
-				Connection con = connect();
-				if (con == null) {
-					return "Error while connecting to the DB for inserting";
-				}
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the DB for inserting";
+			}
 
-				String query = " insert into shedules (`sheduleId`,`lineNo`,`areaNo`,`areaName`,`startTime`,`endTime`,`date`,`reason`)"
-						+ " values (?, ?, ?, ?, ?, ?, ?, ?)";
-				PreparedStatement preparedStat = con.prepareStatement(query);
-				// values binding
-				preparedStat.setInt(1, 0);
-				preparedStat.setString(2, lineno);
-				preparedStat.setString(3, areano);
-				preparedStat.setString(4, areaname);
-				preparedStat.setString(5, starttime);
-				preparedStat.setString(6, endtime);
-				preparedStat.setString(7, date);
-				preparedStat.setString(8, reason);
-				// Execute statement
-	
-				preparedStat.execute();
-				con.close();
-				output = "Power cut schedule details inserted successfully";
+			String query = " insert into shedules (`sheduleId`,`lineNo`,`areaNo`,`areaName`,`startTime`,`endTime`,`date`,`reason`)"
+					+ " values (?, ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStat = con.prepareStatement(query);
+			// values binding
+			preparedStat.setInt(1, 0);
+			preparedStat.setString(2, lineno);
+			preparedStat.setString(3, areano);
+			preparedStat.setString(4, areaname);
+			preparedStat.setString(5, starttime);
+			preparedStat.setString(6, endtime);
+			preparedStat.setString(7, date);
+			preparedStat.setString(8, reason);
+			// Execute statement
+
+			preparedStat.execute();
+			con.close();
+			output = "Power cut schedule details inserted successfully";
 
 		} catch (Exception e) {
 			// output = "Error while inserting power cut shedule details";
@@ -58,48 +58,49 @@ public class Schedule {
 	public String readPowerCutDetails() {
 		String output = "";
 		try {
-				Connection con = connect();
-				if (con == null) {
-					return "Error while connecting to the DB for reading";
-				}
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the DB for reading";
+			}
+
+			// html table to be display
+			output = "<table border='1'><tr><th>Line Number</th><th>Area Number</th><th>Area Name</th>"
+					+ "<th>Start Time</th><th>End Time</th>" + "<th>Date</th><th>Reason</th></tr>";
+			String query = "select * from shedules";
+			Statement stat = con.createStatement();
+			ResultSet rs = stat.executeQuery(query);
+
+			// iteration through the rows of result set
+			while (rs.next()) {
+				String sheduleId = Integer.toString(rs.getInt("sheduleId"));
+				String lineNo = rs.getString("lineNo");
+				String areaNo = rs.getString("areaNo");
+				String areaName = rs.getString("areaName");
+				String startTime = rs.getString("startTime");
+				String endTime = rs.getString("endTime");
+				String date = rs.getString("date");
+				String reason = rs.getString("reason");
 				
-				//html table to be display
-				output = "<table border='1'><tr><th>Line Number</th><th>Area Number</th><th>Area Name</th>"
-						+ "<th>Start Time</th><th>End Time</th>" + "<th>Date</th><th>Reason</th></tr>";
-				String query = "select * from shedules";
-				Statement stat = con.createStatement();
-				ResultSet rs = stat.executeQuery(query);
-				
-				//iteration through the rows of result set
-				while (rs.next()) {
-					String sheduleId = Integer.toString(rs.getInt("sheduleId"));
-					String lineNo = rs.getString("lineNo");
-					String areaNo = rs.getString("areaNo");
-					String areaName = rs.getString("areaName");
-					String startTime = rs.getString("startTime");
-					String endTime = rs.getString("endTime");
-					String date = rs.getString("date");
-					String reason = rs.getString("reason");
-					//add raws into the html table
-					output += "<tr><td>" + lineNo + "</td>";
-					output += "<td>" + areaNo + "</td>";
-					output += "<td>" + areaName + "</td>";
-					output += "<td>" + startTime + "</td>";
-					output += "<td>" + endTime + "</td>";
-					output += "<td>" + date + "</td>";
-					output += "<td>" + reason + "</td>";
-					
-					//buttons
-					output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
-							+ "<td><form method='post' action='shedules.jsp'>"
-							+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
-							+ "<input name='sheduleId' type='hidden' value='" + sheduleId + "'>" + "</form></td></tr>";
-				}
-				
-				con.close();
-				//complete html table
-				output += "</table>";
-	
+				// add raws into the html table
+				output += "<tr><td>" + lineNo + "</td>";
+				output += "<td>" + areaNo + "</td>";
+				output += "<td>" + areaName + "</td>";
+				output += "<td>" + startTime + "</td>";
+				output += "<td>" + endTime + "</td>";
+				output += "<td>" + date + "</td>";
+				output += "<td>" + reason + "</td>";
+
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
+						+ "<td><form method='post' action='shedules.jsp'>"
+						+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+						+ "<input name='sheduleId' type='hidden' value='" + sheduleId + "'>" + "</form></td></tr>";
+			}
+
+			con.close();
+			// complete html table
+			output += "</table>";
+
 		} catch (Exception e) {
 			output = "Error while reading the power cut shedule details.";
 			System.err.println(e.getMessage());
@@ -115,12 +116,13 @@ public class Schedule {
 				return "Error while connecting to the database";
 			}
 			output = "<table border='1'><tr><th>Line No</th>" + "<th>Area No</th>" + "<th>Area Name</th>"
-					+ "<th>Starting Time</th>" + "<th>Ending Time</th>" + "<th>Date</th>" + "<th>Reason</th></tr>"; 
-			
+					+ "<th>Starting Time</th>" + "<th>Ending Time</th>" + "<th>Date</th>" + "<th>Reason</th></tr>";
+
 			// create a prepared statement
 			String query = "select * from shedules where lineNo='" + lineno + "'";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
+			
 			while (rs.next()) {
 				String areano = rs.getString("areano");
 				String areaname = rs.getString("areaname");
@@ -153,11 +155,11 @@ public class Schedule {
 			if (con == null) {
 				return "Error while connecting to the database for updating.";
 			}
-			
+
 			// create a prepared statement
 			String query = "UPDATE shedules SET lineNo=?,areaNo=?, areaName=?, startTime=?, endTime=?, date=?, reason=? WHERE sheduleId=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
-			
+
 			// binding values
 			preparedStmt.setString(1, lineno);
 			preparedStmt.setString(2, areano);
@@ -167,7 +169,7 @@ public class Schedule {
 			preparedStmt.setString(6, date);
 			preparedStmt.setString(7, reason);
 			preparedStmt.setInt(8, Integer.parseInt(ID));
-			
+
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
@@ -186,14 +188,14 @@ public class Schedule {
 			if (con == null) {
 				return "Error while connecting to the database for deleting.";
 			}
-			
+
 			// create a prepared statement
 			String query = "delete from shedules where sheduleId=?";
 			PreparedStatement preparedStmt = con.prepareStatement(query);
-			
-			// binding values 
+
+			// binding values
 			preparedStmt.setInt(1, Integer.parseInt(sheduleId));
-			
+
 			// execute the statement
 			preparedStmt.execute();
 			con.close();
